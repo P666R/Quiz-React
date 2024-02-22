4e6a835e7c4e0cbfcf864c2d2c17c46fe0218215
PRoy <sam469roger@gmail.com>
2024-02-22 11:31:26 +0530

    setup json-server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
-import DateCounter from './DateCounter';
+import { useEffect, useReducer } from 'react';
+import Header from './Header';
+import Main from './Main';
+
+const initialState = {
+  questions: [],
+  //! loading, error, ready, active, finished states
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+
+    case 'loading':
+      return { ...state, status: 'loading' };
+
+    case 'error':
+      return { ...state, status: 'error' };
+
+    case 'ready':
+      return { ...state, status: 'ready' };
+
+    case 'active':
+      return { ...state, status: 'active' };
+
+    case 'finished':
+      return { ...state, status: 'finished' };
+
+    default:
+      throw new Error('Unknown action');
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    async function fetchData() {
+      try {
+        const res = await fetch('http://localhost:8000/questions');
+        const data = await res.json();
+        dispatch({ type: 'dataReceived', payload: data });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1</p>
+        <p>question</p>
+      </Main>
     </div>
   );
 }
