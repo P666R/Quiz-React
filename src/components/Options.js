f36415fe2be7b049beabab6a3379e37a87df4b5c
PRoy <sam469roger@gmail.com>
2024-02-22 13:40:55 +0530

    handling new answers

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,17 @@
-function Options({ options }) {
+function Options({ options, correctOption, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+
   return (
     <div className="options">
-      {options.map((option) => (
-        <button className="btn btn-option" key={option}>
+      {options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? 'answer' : ''} ${
+            hasAnswered ? (index === correctOption ? 'correct' : 'wrong') : ''
+          }`}
+          key={index}
+          disabled={hasAnswered}
+          onClick={() => dispatch({ type: 'newAnswer', payload: index })}
+        >
           {option}
         </button>
       ))}
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,19 @@
 import Options from './Options';
 
-function Question({ question: { question, options, correctOption, points } }) {
+function Question({
+  question: { question, options, correctOption, points },
+  dispatch,
+  answer,
+}) {
   return (
     <div>
       <h4>{question}</h4>
-      <Options options={options} />
+      <Options
+        options={options}
+        correctOption={correctOption}
+        dispatch={dispatch}
+        answer={answer}
+      />
     </div>
   );
 }
